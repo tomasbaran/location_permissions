7ada8692972f1c11cbd417aebb454cde595d9922
Tomas iMac <tomas.baran@gmail.com>
2020-05-06 21:26:10 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/experiment12/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.experiment12
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:location_permissions/location_permissions.dart';
+
+void main() {
+  runApp(MaterialApp(home: MyApp()));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String message = 'unknown';
+  PermissionStatus permission = PermissionStatus.unknown;
+  bool isOpened = false;
+  bool showBottomSheet = false;
+
+  checkPermission() async {
+    permission = await LocationPermissions().checkPermissionStatus();
+    print(
+        '0. permission checked at (${DateTime.now().minute}:${DateTime.now().second}.${DateTime.now().millisecond}): $permission');
+
+    //MARK[blue]: PermissionStatus.unknown
+    if (permission == PermissionStatus.unknown) {
+      try {
+        print(
+            '1. trying to request permission at (${DateTime.now().minute}:${DateTime.now().second}.${DateTime.now().millisecond})');
+        permission = await LocationPermissions().requestPermissions();
+        print(
+            '1. permission requested at (${DateTime.now().minute}:${DateTime.now().second}.${DateTime.now().millisecond}): $permission');
+      } catch (e) {
+        print('LocationPermissions().requestPermissions() error: $e');
+      }
+
+      //MARK[red]: PermissionStatus.denied
+    }
+
+    if (permission == PermissionStatus.denied) {
+      print('1. permission denied');
+      if (!showBottomSheet) {
+        showBottomSheet = true;
+        showModalBottomSheet(
+            isDismissible: false,
+            enableDrag: false,
+            context: context,
+            builder: (BuildContext context) => Container(
+                  height: 500,
+                  child: FlatButton(
+                    onPressed: () async {
+                      isOpened = await LocationPermissions().openAppSettings();
+                      print('isOpened happened');
+                      checkPermission();
+                    },
+                    child: Text('2. Enable Location'),
+                  ),
+                ));
+      } else {
+        await Future.delayed(Duration(milliseconds: 100));
+        checkPermission();
+      }
+      //MARK[green]: PermissionStatus.granted
+    }
+
+    if (permission == PermissionStatus.granted) {
+      if (showBottomSheet) {
+        Navigator.pop(context);
+      }
+      setState(() {
+        message = 'Granted. Yay!!!';
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    checkPermission();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(message),
+      ),
+    );
+  }
+}
